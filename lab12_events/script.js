72946096dbcb795f5594dbf1956ac324c718e94a
naomierrr <122333412+naomierrr@users.noreply.github.com>
2024-06-20 20:54:25 -0400

    Add files via upload

--- lab12_events/script.js
+++ lab12_events/script.js
@@ -96,3 +96,87 @@ window.addEventListener("scroll", function () {
         gotop.style.display = "none"
     }
 })
+
+/*
+- Thursday, June 20
+- FROM EVENTS
+_ INPUT EVENT
+*/
+// get reference to the form elements
+const myform = document.querySelector("#myform")
+const greeting = document.querySelector(".greeting")
+const greetingname = document.querySelector(".greeting p span")
+
+myform.addEventListener("submit", function (event) {
+    //prevent the default form submission
+    event.preventDefault()
+
+    // start form validation, username
+    const usernameinput = document.querySelector("#username")
+    // collect the input text value
+    const username = usernameinput.value
+    //validation 1: make sure the user types a username before pressing the submit button
+    if (username.trim() === "") {
+        //trim is to avoid any white space
+        alert("Please Enter a Username")
+        return; // stop further execution
+    }
+    // if the validation passed, you can submit the data to the serber
+    // greeting message after the validation 
+    greetingname.innerHTML = username
+    greeting.style.display = "block"
+
+    //after the form is submitted, we can clear the username
+    usernameinput.value = ""
+})
+
+/* password validation*/
+//collect form elements
+const passwordfield = document.querySelector("#passwordfield")
+const submitbtn = document.querySelector(".submitbtn")
+// collect the password error message element
+const passworderror = document.querySelector(".passworderror")
+
+window.addEventListener("load", function () {
+    submitbtn.disabled = true
+    submitbtn.style.backgroundColor = "lightgray"
+})
+
+//check the length of the password
+passwordfield.addEventListener("input", function () {
+    let numbercharacter = passwordfield.value.length
+    if (numbercharacter < 8) {
+        passworderror.textContent = "Password must be 8+ characters"
+        passworderror.style.color = "red";
+        passwordfield.style.border = "solid 2px red"
+    }
+    else {
+        passworderror.innerHTML = "&#x2713;"
+        passworderror.style.color = "green";
+        passwordfield.style.border = "solid 2px green"
+        submitbtn.disabled = false
+        submitbtn.style.backgroundColor = "red"
+    }
+})
+
+const comments = document.querySelector("#comments")
+const commenterror = document.querySelector(".commenterror")
+
+comments.addEventListener("input", function () {
+    let charctersLength = comments.value.length
+    if (charctersLength < 10) {
+        commenterror.textContent = "Must be more than 50 words"
+        commenterror.style.color = "red";
+
+    }
+    else if (charctersLength > 20) {
+        commenterror.textContent = "Must less than 2000"
+        commenterror.style.color = "red"
+
+    }
+    else {
+        commenterror.textContent = "ready to submit"
+        commenterror.style.color = "green"
+
+    }
+})
